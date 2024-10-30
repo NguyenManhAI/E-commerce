9b1d3e576d9ba0341fb41fdc2f91d605092ea593
NguyenManh <22022521@vnu.edu.vn>
2024-10-31 01:03:25 +0700

    magento source

---
+++ magento/pub/static/frontend/Magento/luma/en_US/Magento_Csp/js/sri.js
@@ -0,0 +1,1 @@
+/var/www/html/magento/vendor/magento/module-csp/view/base/web/js/sri.js
---
+++ magento/pub/static/frontend/Magento/luma/en_US/PayPal_Braintree/js/model/step-navigator-mixin.js
@@ -0,0 +1,1 @@
+/var/www/html/magento/vendor/paypal/module-braintree-core/view/frontend/web/js/model/step-navigator-mixin.js
