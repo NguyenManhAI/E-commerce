30df355396494fd662a755cfda465f0a78d69987
To Huu Bang <120768537+snozywolf59@users.noreply.github.com>
2024-12-19 23:58:16 +0700

    cache 1 gio vnexpress

--- app/code/Bang/News/Helper/Data.php
+++ app/code/Bang/News/Helper/Data.php
@@ -3,15 +3,33 @@
 namespace Bang\News\Helper;
 
 use Bang\News\Model\RssReader;
+use Magento\Framework\App\Helper\AbstractHelper;
+use Magento\Framework\App\Cache\TypeListInterface;
+use Magento\Framework\App\Cache\Frontend\Pool;
 
-class Data extends \Magento\Framework\App\Helper\AbstractHelper {
-    private const newsUrl = "https://vnexpress.net/rss/kinh-doanh.rss";
+class Data extends AbstractHelper
+{
+    private const NEWS_URL = "https://vnexpress.net/rss/kinh-doanh.rss";
     private const ITEMS_PER_PAGE = 5;
-    protected $newsList = [];
+    private const CACHE_KEY = 'vnexpress_news_cache';
+    private const CACHE_LIFETIME = 3600; // Cache for 1 hour
 
-    public function getNews($page = 1) {
-        $this->newsList = [];
-        $allNews = RssReader::loadRss(self::newsUrl);
+    protected $newsList = [];
+    protected $cache;
+    protected $cacheKey;
+
+    public function __construct(
+        \Magento\Framework\App\Helper\Context $context,
+        Pool $cacheFrontendPool
+    ) {
+        parent::__construct($context);
+        $this->cache = $cacheFrontendPool->get('default');
+        $this->cacheKey = self::CACHE_KEY;
+    }
+
+    public function getNews($page = 1)
+    {
+        $allNews = $this->getCachedNews();
         
         // Calculate pagination
         $totalItems = count($allNews);
@@ -27,5 +45,28 @@ class Data extends \Magento\Framework\App\Helper\AbstractHelper {
             'totalPages' => $totalPages,
             'currentPage' => $page
         ];
+    }
+
+    protected function getCachedNews()
+    {
+        // Try to get news from cache
+        $cachedData = $this->cache->load($this->cacheKey);
+        
+        if ($cachedData) {
+            return unserialize($cachedData);
+        }
+
+        // If cache is empty or expired, load from RSS
+        $allNews = RssReader::loadRss(self::NEWS_URL);
+        
+        // Save to cache
+        $this->cache->save(
+            serialize($allNews),
+            $this->cacheKey,
+            [],
+            self::CACHE_LIFETIME
+        );
+
+        return $allNews;
     }
 }
